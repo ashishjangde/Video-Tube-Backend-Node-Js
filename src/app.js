60791061449fc72b0c1e4cc6fd062b5e45a9b8cb
Ashish Jangde <ashishjangde2003@gmail.com>
2024-09-11 16:55:22 +0530

    setting up rotes and multer and cloudinary

--- src/app.js
+++ src/app.js
@@ -7,12 +7,18 @@ const app = express();
 app.use(express.json({limit: "50kb"}));
 app.use(express.urlencoded({ extended: true , limit: "50kb"}));
 app.use(express.static("public"));
-
 app.use(cookieParser());
-
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true
 }));
+
+//routes import
+import userRoutes from "./routes/user.routes.js";
+
+//routes declaration
+app.use(`${process.env.DEFAULT_URL}/users`, userRoutes);
+
+
 
 export{app}
